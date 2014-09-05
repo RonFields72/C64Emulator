a606c1e62163d4f7e502ede3a1630845ad71eaaa
RonFields72 <eightbits72@gmail.com>
2014-09-05 14:44:52 -0500

    Initial commit.

--- c64_1541ii/CBM1541.cs
+++ c64_1541ii/CBM1541.cs
@@ -10,7 +10,7 @@
  * 
  * C#64 - Commodore 64 Emulator written in C#
  * Copyright (C) 2014  Mladen Janković
- *
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -24,6 +24,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
+ * Ron Fields 09/05/2014
  */
 
 namespace DiskDrive
